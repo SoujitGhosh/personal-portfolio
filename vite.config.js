71bce17de1002e0403a33576f8f30ddd569b9fcd
SoujitGhosh <soujitghosh2003@gmail.com>
2025-10-27 16:04:04 +0530

    Fix alias config for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url"; // ✅ correct spelling
+import tailwindcss from "tailwindcss";
+import { fileURLToPath, URL } from "node:url";
 
-// ✅ Proper ESM replacements for __filename and __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-},
-},
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
